db0d4076b3f5b8740ef08c07981db90dcd4fff26
Yisel Gari <yiselgari@gmail.com>
2023-09-20 13:43:55 -0300

    add scritp for run experiments and analysis

---
+++ scripts/run-experiments.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+#cleaning directories [logs, qTables, qTablesSum, results]
+sh scripts/clean.sh
+
+#running experiments
+python scripts/simulate.py 4 casesRL_train.csv
+
+#running analyses
+java -Xmx256m -cp "./cloudautoscaling-1.0.jar" com.cloudautoscaling.Step2_LaunchAnalyses
+
+echo "Experiments completed, check outputDirectory: results"
+
+
